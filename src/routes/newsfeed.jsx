972f18cce4bcefddf765038be2a588faf458528a
RM_tech_1980 <marco.ruzzo06@gmail.com>
2024-01-25 23:18:38 +0100

    25.01.24  23:17 Uhr div. Änderungen in logingoogle newacc newsfeed

--- src/routes/newsfeed.jsx
+++ src/routes/newsfeed.jsx
@@ -1,13 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import '../newsfeed.css';
-import newsfeedItem from 'src/routes/components/newsfeedItem.jsx';
-
-const UserProfile = ({ username }) => (
-  <div className="user-profile">
-    <img src="default-profile-picture.jpg" alt={`${username}'s profile`} />
-    <p>{username}</p>
-  </div>
-);
 
 const Reactions = ({ reactions }) => (
   <div className="reactions">
@@ -22,9 +14,9 @@ const NewsFeed = () => {
   const [newMessage, setNewMessage] = useState('');
   const [uploadedImage, setUploadedImage] = useState(null);
   const [userData, setUserData] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    // Hier fügst du den Code ein, um die Nutzerdaten vom Backend abzurufen
     const fetchUserData = async () => {
       try {
         const response = await fetch('https://845d97vw4k.execute-api.eu-central-1.amazonaws.com/getAllPosts', {
@@ -40,11 +32,14 @@ const NewsFeed = () => {
 
         if (data.status === 'ok') {
           setUserData(data);
+          setMessages(data.posts); // Speichere die empfangenen Nachrichten
         } else {
           console.error('Fehler beim Abrufen der Nutzerdaten', data);
         }
       } catch (error) {
         console.error('Fehler beim Netzwerkaufruf', error);
+      } finally {
+        setIsLoading(false);
       }
     };
 
@@ -52,34 +47,32 @@ const NewsFeed = () => {
   }, []); 
 
   const handleSendMessage = async () => {
-    if ((newMessage.trim() !== '' || uploadedImage) && username.trim() !== '') {
-      try {
-        const response = await fetch('https://845d97vw4k.execute-api.eu-central-1.amazonaws.com/addPost', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
-            user_id: userData.userId, // Nutzer-ID aus den abgerufenen Daten
-            content: newMessage,
-            media_link: uploadedImage,
-          }),
-        });
+    try {
+      const response = await fetch('https://845d97vw4k.execute-api.eu-central-1.amazonaws.com/addPost', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          user_id: localStorage.getItem("UserID"),
+          content: newMessage,
+          media_link: uploadedImage,
+        }),
+      });
 
-        const data = await response.json();
+      const data = await response.json();
 
-        console.log('Data:', data);
+      console.log('Data:', data);
 
-        if (data.status === 'ok') {
-          setMessages([...messages, data.savedPost]);
-          setNewMessage('');
-          setUploadedImage(null);
-        } else {
-          console.error('Fehler beim Speichern der Nachricht');
-        }
-      } catch (error) {
-        console.error('Fehler beim Netzwerkaufruf', error);
+      if (data.status === 'ok') {
+        setMessages([...messages, data.savedPost]);
+        setNewMessage('');
+        setUploadedImage(null);
+      } else {
+        console.error('Fehler beim Speichern der Nachricht');
       }
+    } catch (error) {
+      console.error('Fehler beim Netzwerkaufruf', error);
     }
   };
 
@@ -97,15 +90,21 @@ const NewsFeed = () => {
       <img src="https://cdn.discordapp.com/attachments/1195301143161606205/1195301598507827240/techst_logo_rz_white.png?ex=65b37e5c&is=65a1095c&hm=951cba6cabd865ab2f4e7c4fd8e295c18bb4f3b9a3474d434849184a84fcbd48&" alt="Logo" className="logo" />
       <div className="chat">
         <div className="messages">
-          {messages.slice(-1000).map((message, index) => (
-            <div className="message" key={index}>
-              <UserProfile username={message.username} />
-              <p>{message.text}</p>
-              {message.image && <img src={message.image} alt="Uploaded" />}
-              {message.timestamp && <p>Posted at: {new Date(message.timestamp).toLocaleTimeString()}</p>}
-              <Reactions reactions={message.reactions || []} />
-            </div>
-          ))}
+          {isLoading ? (
+            <p>Lädt Nachrichten...</p>
+          ) : (
+            messages.slice(-1000).map((message, index) => (
+              <div className="message" key={index}>
+                {/* Hier können die Nachrichtenkomponenten gerendert werden */}
+                {/* <UserProfile username={message.username} /> */}
+                <p>{message.content}</p>
+                <img src={message.MediaLink} alt="Uploaded" width='600px'/>
+                {/* {message.image && <img src={message.image} alt="Uploaded" />} */}
+                {/* {message.timestamp && <p>Posted at: {new Date(message.timestamp).toLocaleTimeString()}</p>} */}
+                {/* <Reactions reactions={message.reactions || []} /> */}
+              </div>
+            ))
+          )}
         </div>
         <div className="input-container">
           <input
