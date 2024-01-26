5f57af925c0108d3745226ad7470cdaffa8288b2
Chris <christian.altenhofer@gmx.de>
2024-01-26 12:54:41 +0100

    26.01.2024, 13 Uhr, aktuelle Daten ziehen

--- src/routes/logingoogle.jsx
+++ src/routes/logingoogle.jsx
@@ -43,24 +43,29 @@ function LoginGoogle() {
       );
 
       const responseData = await responseToBackend.json();
-      console.log("data From backend login google",responseData)
+      console.log(responseData)
       
       setUserData(responseData);
-
-      ///   SessionData und UserID ins LocalStorage speichern ///
       var sessionData = responseData.sessionData;
+      
       localStorage.setItem("Session",sessionData );
+      
 
       var existingUserMessage = JSON.parse(responseData.steps.existingUserMessage);
+
+      // Extract UserID from the parsed object
       var userID = existingUserMessage[0].UserID;
+      
+      // Log the UserID to the console
+      console.log("UserID:", userID);
       localStorage.setItem("UserID",userID );
 
        
       if (responseData.isNewUser === false) {
-        navigate("/newsfeed");
+        navigate("/newacc");
       }
       else {
-        navigate("/newacc");
+        navigate("/newsfeed");
       }
 
     } catch (error) {
