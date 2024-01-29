47eb9b385ee01ae62215c1896cd8a5fd605c8040
Chris <christian.altenhofer@gmx.de>
2024-01-29 10:35:38 +0100

    29.01.24 - 10:35 Uhr, Timestamp newsfeed

--- src/routes/newsfeed.jsx
+++ src/routes/newsfeed.jsx
@@ -1,31 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import '../newsfeed.css';
-//import newsfeedItem from 'src/routes/components/newsfeedItem.jsx';
-
-const UserProfile = ({ username }) => (
-  <div className="user-profile">
-    <img src="default-profile-picture.jpg" alt={`${username}'s profile`} />
-    <p>{username}</p>
-  </div>
-);
-
-const Reactions = ({ reactions }) => (
-  <div className="reactions">
-    {reactions.map((reaction, index) => (
-      <span key={index}>{reaction.emoji} by {reaction.username}</span>
-    ))}
-  </div>
-);
 
 const NewsFeed = () => {
   const [messages, setMessages] = useState([]);
   const [newMessage, setNewMessage] = useState('');
   const [uploadedImage, setUploadedImage] = useState(null);
-  const [userData, setUserData] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    // Hier fügst du den Code ein, um die Nutzerdaten vom Backend abzurufen
-    const fetchUserData = async () => {
+    const fetchMessages = async () => {
       try {
         const response = await fetch('https://845d97vw4k.execute-api.eu-central-1.amazonaws.com/getAllPosts', {
           method: 'GET',
@@ -33,53 +16,53 @@ const NewsFeed = () => {
             'Content-Type': 'application/json',
           },
         });
-
         const data = await response.json();
-
-        console.log(data);
-
         if (data.status === 'ok') {
-          setUserData(data);
+          setMessages(data.posts);
         } else {
-          console.error('Fehler beim Abrufen der Nutzerdaten', data);
+          console.error('Fehler beim Abrufen der Nachrichten', data);
         }
       } catch (error) {
         console.error('Fehler beim Netzwerkaufruf', error);
+      } finally {
+        setIsLoading(false);
       }
     };
 
-    fetchUserData();
-  }, []); 
+    fetchMessages();
+  }, []);
 
   const handleSendMessage = async () => {
-    if ((newMessage.trim() !== '' || uploadedImage) && username.trim() !== '') {
-      try {
-        const response = await fetch('https://845d97vw4k.execute-api.eu-central-1.amazonaws.com/addPost', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
-            user_id: userData.userId, // Nutzer-ID aus den abgerufenen Daten
-            content: newMessage,
-            media_link: uploadedImage,
-          }),
-        });
+    if (!newMessage.trim()) return; // Verhindert das Senden leerer Nachrichten
+    try {
+      const response = await fetch('https://845d97vw4k.execute-api.eu-central-1.amazonaws.com/addPost', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          user_id: localStorage.getItem("UserID"),
+          content: newMessage.trim(),
+          media_link: uploadedImage,
+        }),
+      });
 
-        const data = await response.json();
+      const data = await response.json();
 
-        console.log('Data:', data);
-
-        if (data.status === 'ok') {
-          setMessages([...messages, data.savedPost]);
-          setNewMessage('');
-          setUploadedImage(null);
-        } else {
-          console.error('Fehler beim Speichern der Nachricht');
-        }
-      } catch (error) {
-        console.error('Fehler beim Netzwerkaufruf', error);
+      if (data.status === 'ok') {
+        // Setze die Nachrichtenliste neu und füge die neue Nachricht hinzu
+        setMessages([...messages, {
+          content: newMessage,
+          MediaLink: uploadedImage,
+          timestamp: new Date() // Hier wird die aktuelle Zeit als Timestamp genommen
+        }]);
+        setNewMessage(''); // Nachrichtenfeld leeren
+        setUploadedImage(null); // Zurücksetzen des Uploads
+      } else {
+        console.error('Fehler beim Senden der Nachricht', data);
       }
+    } catch (error) {
+      console.error('Fehler beim Netzwerkaufruf', error);
     }
   };
 
@@ -94,18 +77,21 @@ const NewsFeed = () => {
 
   return (
     <div className="app">
-      <img src="https://cdn.discordapp.com/attachments/1195301143161606205/1195301598507827240/techst_logo_rz_white.png?ex=65b37e5c&is=65a1095c&hm=951cba6cabd865ab2f4e7c4fd8e295c18bb4f3b9a3474d434849184a84fcbd48&" alt="Logo" className="logo" />
+      <img src="https://cdn.discordapp.com/attachments/1195301143161606205/1195301598507827240/techst_logo_rz_white.png" alt="Logo" className="logo" />
       <div className="chat">
         <div className="messages">
-          {messages.slice(-1000).map((message, index) => (
-            <div className="message" key={index}>
-              <UserProfile username={message.username} />
-              <p>{message.text}</p>
-              {message.image && <img src={message.image} alt="Uploaded" />}
-              {message.timestamp && <p>Posted at: {new Date(message.timestamp).toLocaleTimeString()}</p>}
-              <Reactions reactions={message.reactions || []} />
-            </div>
-          ))}
+          {isLoading ? (
+            <p>Lädt Nachrichten...</p>
+          ) : (
+            messages.slice(-1000).map((message, index) => (
+              <div className="message" key={index}>
+                <p>{message.content}</p>
+                {message.MediaLink && <img src={message.MediaLink} alt="Uploaded" width='600px' />}
+                {/* Anzeige des Zeitstempels */}
+                <span className="timestamp">{message.timestamp ? new Date(message.timestamp).toLocaleTimeString() : 'Zeit unbekannt'}</span>
+              </div>
+            ))
+          )}
         </div>
         <div className="input-container">
           <input
