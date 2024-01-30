645607a6bf0952e296cf8f8f0f7b814ffc6d8ce0
Ramzi <atrous.ramzi@gmx.de>
2024-01-30 21:41:21 +0100

    prod uri und pipeline

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -5,7 +5,10 @@ import '../App.css';
 function Root() {
   // Diese Funktion wird aufgerufen, wenn der Benutzer auf "Login with Google" klickt
   const handleGoogleLoginClick = () => {
-    window.location.href = "https://accounts.google.com/o/oauth2/v2/auth?scope=openid+https://www.googleapis.com/auth/userinfo.email+https://www.googleapis.com/auth/userinfo.profile&include_granted_scopes=true&response_type=token&state=state_parameter_passthrough_value&redirect_uri=https://aws2302.atrous.de/login/google&client_id=102702819791-f66fq0h8o2mq4kn0t8498gar2746icgv.apps.googleusercontent.com";
+    const url_dev = "https://accounts.google.com/o/oauth2/v2/auth?scope=openid+https://www.googleapis.com/auth/userinfo.email+https://www.googleapis.com/auth/userinfo.profile&include_granted_scopes=true&response_type=token&state=state_parameter_passthrough_value&redirect_uri=http://localhost:5173/login/google&client_id=102702819791-f66fq0h8o2mq4kn0t8498gar2746icgv.apps.googleusercontent.com";
+    ///production///
+    const url_prod = "https://accounts.google.com/o/oauth2/v2/auth?scope=openid+https://www.googleapis.com/auth/userinfo.email+https://www.googleapis.com/auth/userinfo.profile&include_granted_scopes=true&response_type=token&state=state_parameter_passthrough_value&redirect_uri=https://aws2302.atrous.de/login/google&client_id=102702819791-f66fq0h8o2mq4kn0t8498gar2746icgv.apps.googleusercontent.com";
+    window.location.href = url_prod;
   };
 
   return (
